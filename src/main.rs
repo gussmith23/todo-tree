f7295a462c8f87f9e92ed1c47686cebe3e5a2936
Collin Baker <chbaker0@gmail.com>
2018-09-09 13:34:40 -0700

    Create basic Rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Nothing here."
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Serve index() at http://localhost:8000/
+    rocket::ignite().mount("/", routes![index]).launch();
 }
