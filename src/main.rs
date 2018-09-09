14d8e5aa138910b17b29304128d83475394fea28
Collin Baker <chbaker0@gmail.com>
2018-09-09 13:55:53 -0700

    Test mutable state in Rocket

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@
 extern crate rocket;
 
 use rocket as rkt;
+use std::sync::Mutex;
+
+struct ServerState {
+    hit_count: Mutex<usize>
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Nothing here."
+fn index(state: rkt::State<ServerState>) -> String {
+    let old_count = {
+        let mut hit_count = state.hit_count.lock().unwrap();
+        *hit_count += 1;
+        *hit_count - 1
+    };
+
+    format!("Server hit {} times since start.", old_count)
 }
 
 fn main() {
-    // Serve index() at http://localhost:8000/
-    rkt::ignite().mount("/", routes![index]).launch();
+    // Manage state and serve index() at http://localhost:8000/
+    rkt::ignite()
+        .manage(ServerState { hit_count: Mutex::new(0) })
+        .mount("/", routes![index]).launch();
 }
