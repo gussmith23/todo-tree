af72c91056d33b5ba54aa8578e8fb1a513041dfd
Anthony Marucci <amarucci13@gmail.com>
2018-09-13 21:56:53 -0700

    Add endpoints to delete and update todo lists

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,23 @@ struct ServerState {
     todo_list_store: Mutex<InMemoryStore>,
 }
 
+#[delete("/lists/<id>", format = "application/json")]
+fn delete_list(state: rkt::State<ServerState>, id: u64) -> Option<()> {
+    let mut list_store = state.todo_list_store.lock().unwrap();
+    list_store.delete(TodoListId(id)).ok()
+}
+
+#[put("/lists/<id>", format = "application/json", data = "<title>")]
+fn update_list(state: rkt::State<ServerState>, id: u64, title: String) -> Option<()> {
+    let todo_list_id = TodoListId(id);
+    let mut list_store = state.todo_list_store.lock().unwrap();
+    let todo_list = TodoList {
+        title: title.to_string(),
+        entries: Default::default(),
+    };
+    list_store.update(todo_list_id, &todo_list).ok()
+}
+
 #[get("/lists/<id>", format = "application/json")]
 fn get_list(state: rkt::State<ServerState>, id: u64) -> Option<String> {
     let todo_list_id = TodoListId(id);
@@ -27,7 +44,7 @@ fn get_list(state: rkt::State<ServerState>, id: u64) -> Option<String> {
 }
 
 #[post("/lists", format = "text/plain", data = "<title>")]
-fn create(state: rkt::State<ServerState>, title: String) -> String {
+fn create_list(state: rkt::State<ServerState>, title: String) -> String {
     let todo_list = TodoList {
         title: title.to_string(),
         entries: Default::default(),
@@ -42,7 +59,10 @@ fn rocket() -> rocket::Rocket {
     rkt::ignite()
         .manage(ServerState {
             todo_list_store: Mutex::new(InMemoryStore::new()),
-        }).mount("/", routes![create, get_list])
+        }).mount(
+            "/",
+            routes![create_list, get_list, update_list, delete_list],
+        )
 }
 
 fn main() {
@@ -90,5 +110,51 @@ mod tests {
 
         assert_eq!(response1.status(), Status::Ok);
         assert_eq!(response2.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn test_update() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+
+        client
+            .post("/lists")
+            .body("title=abc")
+            .header(ContentType::Plain)
+            .dispatch();
+        let response1 = client
+            .put(format!("/lists/{}", 0))
+            .body("title=xyz")
+            .header(ContentType::JSON)
+            .dispatch();
+        let response2 = client
+            .put(format!("/lists/{}", 9))
+            .body("title=xyz")
+            .header(ContentType::JSON)
+            .dispatch();
+
+        assert_eq!(response1.status(), Status::Ok);
+        assert_eq!(response2.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn test_delete() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+
+        client
+            .post("/lists")
+            .body("title=abc")
+            .header(ContentType::Plain)
+            .dispatch();
+        client
+            .delete(format!("/lists/{}", 0))
+            .body("title=xyz")
+            .header(ContentType::JSON)
+            .dispatch();
+        let response = client
+            .get(format!("/lists/{}", 0))
+            .header(ContentType::Plain)
+            .dispatch();
+
+        assert_eq!(response.status(), Status::NotFound);
     }
 }
