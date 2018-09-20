c40ea8e0caf48a1c0c06908bb00a29ab0854338c
Collin Baker <chbaker0@gmail.com>
2018-09-19 22:17:04 -0700

    christ

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,5 +1,6 @@
 set -e
 
+openssl aes-256-cbc -K $encrypted_3d130dceec06_key -iv $encrypted_3d130dceec06_iv -in compute-engine-creds.json.enc -out compute-engine-creds.json -d
 curl https://sdk.cloud.google.com | CLOUDSDK_INSTALL_DIR=$HOME CLOUDSDK_CORE_DISABLE_PROMPTS=1 bash
 mkdir -p lib
 gcloud auth activate-service-account --key-file compute-engine-creds.json
